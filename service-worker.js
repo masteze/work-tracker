289a75ccdaee6591ad102a4fc935e78d17a6d6d3
Александр Сажин <SazhinAY@sbm.local>
2024-12-09 21:25:24 +0500

    Deploy Blazor app

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+/* Manifest version: DDx7kQjP */
+// In development, always fetch from the network and do not enable offline support.
+// This is because caching would make development more difficult (changes would not
+// be reflected on the first load after each change).
+self.addEventListener('fetch', () => { });
+
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing.');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('Fetching:', event.request.url);
+});
